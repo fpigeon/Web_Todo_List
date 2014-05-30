a1268bdf6dd90f33e9b55f64969919c14586b81d
Frank Pigeon <frank.pigeonjr@gmail.com>
2014-05-30 13:16:22 -0500

    added an upload file and adds to do the existing list

--- public/todo_List.php
+++ public/todo_List.php
@@ -11,7 +11,7 @@
 	$todos = [];
 	$file_path='data/todo.txt';
 
-	//functions
+	//functions	
 	function saveFile($filename, $list_array){
 	    if($filename == ''){
 	        $filename='data/default.txt';    
@@ -70,7 +70,7 @@
 	}
 
 	//remove item
-	else if (isset($_GET['remove_item']) ){
+	if (isset($_GET['remove_item']) ){
 		 $removeItem = $_GET['remove_item'];
 		 var_dump($removeItem);
 		 unset($todos[$removeItem]);
@@ -78,6 +78,29 @@
 		 header('Location: /todo_List.php');
 		 exit(0);
 	} //end of remove item
+	//saveFile($file_path, $todos); // save your file
+
+	//move uploaded files to the upload directory
+	if (count($_FILES) > 0 && $_FILES['file1']['error'] == 0) {
+    // Set the destination directory for uploads
+    $upload_dir = '/vagrant/sites/todo.dev/public/uploads/';
+    // Grab the filename from the uploaded file by using basename
+    $filename = basename($_FILES['file1']['name']);
+    // Create the saved filename using the file's original name and our upload directory
+    $saved_filename = $upload_dir . $filename;
+    // Move the file from the temp location to our uploads directory
+    move_uploaded_file($_FILES['file1']['tmp_name'], $saved_filename);
+	} // end of upload files
+
+	// Check if we saved a file
+	if (isset($saved_filename)) {
+	    // If we did, load todo items in to the todo arrays
+	 	$saved_file_items = open_file($saved_filename);
+	    foreach ($saved_file_items as $list_item) {
+	        array_push($todos, $list_item); //add to the end of the array
+	    } //end of foreach
+	} // add to the array if found
+
 	?>
 
 	<!-- output array on screen -->
@@ -92,13 +115,23 @@
 	</ul>
 	
 	<h2>Input New Todo Items</h2>
-	<form method="POST">
+	<form method="POST" action="/todo_List.php">
 		<!-- <label for="item_num">Item Number: </label>
         <input id="item_num" name="item_num" type="text" placeholder="Todo Number">
         <br> -->
         <label for="task">Task</label>
         <input id="task" name="task" type="text" placeholder="Add Todo Item">
         <button type="submit">Add Item</button>
+	</form>
+
+	<h2>Upload File</h2>
+
+	<form method="POST" enctype="multipart/form-data">
+	    <label for="file1">File to upload: </label>
+	    <input type="file" id="file1" name="file1">
+		<br>
+	    <input type="submit" value="Upload">
+    
 	</form>
 	
 	
