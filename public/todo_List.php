ed33a32e88a43583da2b9a00aba217db236c3729
Frank Pigeon <frank.pigeonjr@gmail.com>
2014-06-18 19:00:50 -0500

    added a filestore class

--- public/todo_List.php
+++ public/todo_List.php
@@ -1,72 +1,16 @@
-<? 
+<?
+//include classes
+require_once('classes/filestore.php');
 //variables
-$todos = [];  //array to hold todo items
-$file_path='data/todo.txt'; //local text file
+$todos = [];  //array to hold todo item
 $error_msg=''; //initailize variable to hold error messages
 
-/******************************************************************************
-FUNCTION:	    saveFile
-  DESCRIPTION:  saves todo items to local text file via write
-  INPUT:		
-     Parameters: $filename - path to data file where we are saving
-     			 $list_array - array that contains todo items	
-	 File: default is data/todo.txt
-  OUTPUT:
-	 Return Val: Boolean True if save successful, False if there was an error
-     Parameters:	none
-	 File: default is data/todo.txt
-  CALLS TO:	none
-******************************************************************************/	
-function saveFile($filename, $list_array){
-	if($filename == ''){
-	    $filename='data/todo.txt';
-	} //if user just hits enter
-	$handle = fopen($filename, 'w');
-	if (is_writeable($filename)){        
-	    foreach ($list_array as $list_item) {
-	        fwrite($handle, $list_item . PHP_EOL);
-	    }//end of foreach
-	    fclose($handle);
-	    return TRUE;
-	} //end of ovewrite ok
-	else {
-	    return FALSE;
-	} // end of else
-} //end of SaveFile
+//create a new data store based on filestore
+$todo_store = new Filestore();
 
-/******************************************************************************
-FUNCTION:	    open_file
-  DESCRIPTION:  opens the default local todo and reads the content
-  INPUT:		
-     Parameters: $filename - path to data file where we are saving
-	 File: default is data/todo.txt
-  OUTPUT:
-	 Return Val: array
-     Parameters: $arrayed - array of todo items
-	 File: None
-  CALLS TO:	none
-******************************************************************************/
-function open_file($filename){
-    if($filename == ''){
-        $filename='data/todo.txt';    
-    } //if user just hits enter
-
-    if ( (is_readable($filename) && (filesize($filename) > 0))) {
-        $handle = fopen($filename, 'r');
-        $contents = trim(fread($handle, filesize($filename)));
-        fclose($handle);
-        //echo $contents;    
-        $arrayed = explode(PHP_EOL, $contents);
-        return $arrayed; 
-    }//end of file found
-    else {
-        echo 'Error Reading File' . PHP_EOL;
-        return FALSE;
-    }//file not found
-}//end of open file	
-	
 //go thru the local text file and add to the array
-$file_items = open_file($file_path);
+$file_items = $todo_store->read_lines();
+
 if ($file_items !== FALSE){
     foreach ($file_items as $list_item) {
         array_push($todos, $list_item); //add to the end of the array
@@ -76,8 +20,8 @@ if ($file_items !== FALSE){
 //add new todo items from POST
 if (!empty($_POST['task'])){	
 	$newTodo = $_POST['task']; //assign the variable from the post
-	$todos[] = $newTodo; // add to the array
-	saveFile($file_path, $todos); // save your file
+	$todos[] = $newTodo; // add to the array	
+	$todo_store->write_lines($todos); // save your file
 	header('Location: /todo_List.php');
 	exit(0);
 } //end of add item
@@ -85,8 +29,8 @@ if (!empty($_POST['task'])){
 //remove item from todo array using GET
 if (isset($_GET['remove_item']) ){
 	 $removeItem = $_GET['remove_item'];	 
-	 unset($todos[$removeItem]); //remove from todo array
-	 saveFile($file_path, $todos); // save your file
+	 unset($todos[$removeItem]); //remove from todo array	 
+	 $todo_store->write_lines($todos); // save your file
 	 header('Location: /todo_List.php');
 	 exit(0);
 } //end of remove item
@@ -102,12 +46,12 @@ if (count($_FILES) > 0 && $_FILES['file1']['error'] == 0) {
 	    // Move the file from the temp location to our uploads directory
 	    move_uploaded_file($_FILES['file1']['tmp_name'], $saved_filename);
 
-	    //add items to the todo list
-	    $saved_file_items = open_file($saved_filename);
+	    //add items to the todo list	    
+	    $saved_file_items = $todo_store->read_lines($saved_filename);
 	    foreach ($saved_file_items as $list_item) {
 	        array_push($todos, $list_item); //add to the end of the array
-	    } //end of foreach
-	    saveFile($file_path, $todos); // save your file	
+	    } //end of foreach	    
+	    $todo_store->write_lines($todos); // save your file	
 	} // Set the destination directory for uploads
     else{
     	$error_msg = 'Upload error: wrong file type. Must be .txt';
@@ -126,8 +70,7 @@ if (count($_FILES) > 0 && $_FILES['file1']['error'] == 0) {
 	<h1>TODO List</h1>	 
 	<? if(!empty($error_msg)) : ?>
 		<?= PHP_EOL . $error_msg . PHP_EOL;?>
-	<? endif; ?>
-	
+	<? endif; ?>	
 	
 	<!-- output array on screen -->
 	<ul>	 	
@@ -142,7 +85,7 @@ if (count($_FILES) > 0 && $_FILES['file1']['error'] == 0) {
 	<form method="POST" action="/todo_List.php">		
         <label for="task">Task</label>
         <input id="task" name="task" type="text" placeholder="Add Todo Item">
-        <button type="submit">Add Item</button>
+        <button type="submit" class="button">Add Item</button>
 	</form>
 
 	<h2>Upload File</h2>
@@ -150,7 +93,7 @@ if (count($_FILES) > 0 && $_FILES['file1']['error'] == 0) {
 	    <label for="file1">File to upload: </label>
 	    <input type="file" id="file1" name="file1">
 		<br>
-	    <input type="submit" value="Upload">    
+	    <input type="submit" value="Upload" class="button">    
 	</form>	
 </body>
 </html>
